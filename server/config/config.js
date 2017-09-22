36c6963ca149bb1047eca199adf63cd877fe586a
CharlesElvira <ninou14fr@ufl.edu>
2017-09-22 19:03:03 -0400

    trye new

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   db: {
-    uri: process.env.MONGODB_URI,
+    uri: 'mongodb://root:root@ds147044.mlab.com:47044/heroku_w78mxshr',
   }, 
   googleMaps: {
     key: 'AIzaSyDY8_g992gMYw8BdIYzu1Fjov0u097n3uA'
